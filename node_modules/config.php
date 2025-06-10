d937071c4c9ec24a1eefa6791a8f49f872e96ea8
Carlitos <juanpana10ospina@gmail.com~>
2025-06-10 15:47:36 -0500

    Gitlens

---
+++ node_modules/config.php
@@ -0,0 +1,18 @@
+const Fugue = {
+    Exposicion : {
+        Tema : "T",
+        ContraTema : ["CT", "CT2"],
+        Entrada_Complementaria : "EC1"
+    },
+    Episodio : "",
+    Desarrollo : {
+        Tema : "T",
+        ContraTema : ["CT", "CT2"],
+    },
+    Re_Exposicion : {
+        Tema : "T",
+        ContraTema : ["CT", "CT2"]
+    },
+    Coda : "C"
+}
+console.log(Fugue);
