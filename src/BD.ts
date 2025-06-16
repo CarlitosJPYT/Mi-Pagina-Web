aea98ce7d85b95021ab2550e2b3786657335238c
Carlitos <juanpana10ospina@gmail.com~>
2025-06-16 09:32:54 -0500

    Merge

--- src/BD.ts
+++ src/BD.ts
@@ -1,5 +1,23 @@
-const fugue = require('db.ts');
-const Requiem = require('database.ts');
-
-console.log(fugue);
+const Requiem = {
+    Introitus : ["Requiem", "Aeternam"],
+    Kirye : "Eleison",
+    Sequentia : {
+        Dies : "Irae",
+        Tuba : "Mirum",
+        Rex : ["Tremendae", "Majestatis"],
+        Recordare : ["Jesu", "Pie"],
+        Confutatis : "Maledictus",
+        Lacrimosa : ["Dies", "Illa"],
+    },
+    Offertorium : {
+        Domine : ["Jesu", "Christe"],
+        Versus : ["Hostias", "Et"]
+    },
+    Sanctus : {
+        Sanctus : {Dominus : ["Deus", "Sabaoth"]},
+        Benedictus : ""
+    },
+    Agnus : "Dei",
+    Communio : ["Lux", "Aeterna"]
+}
 console.log(Requiem);
