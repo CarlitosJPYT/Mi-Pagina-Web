c720ccc40f213cbc7fb2b7d9a532a7e76f7e3f57
Carlitos <juanpana10ospina@gmail.com~>
2025-06-20 18:31:13 -0500

    Change

---
+++ public/main.py
@@ -0,0 +1,18 @@
+Fugue = {
+    "Exposicion": {
+        "Tema" : "T",
+        "ContraTema": ["CT", "CT2"],
+        "Entrada_Complementaria": "EC",
+    },
+    "Episodio": "",
+    "Desarrollo": {
+        "Tema": "T",
+        "ContraTema": ["CT", "CT2"]
+    },
+    "Episodio": "",
+    "Re_Exposicion": {
+        "Tema": "T",
+        "ContraTema": ["CT", "CT2"]
+    },
+    "Coda": "EC"
+}
